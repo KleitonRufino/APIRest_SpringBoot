7725212dbcc13d243c208c0859845771509e58ed
Rufino <kleiton.rufino@capgemini.com>
2020-11-16 19:37:39 -0300

    commit

---
+++ 00 apirest_springboot/src/main/java/br/com/apirest_springboot/Startup.java
@@ -0,0 +1,12 @@
+package br.com.apirest_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+}
