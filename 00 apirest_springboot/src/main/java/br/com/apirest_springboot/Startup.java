ac6b02970a432eebe390ab3eaa38b6b38230e108
Rufino <kleiton.rufino@capgemini.com>
2020-11-23 17:12:45 -0300

    add servico de arquivos/file

--- 00 apirest_springboot/src/main/java/br/com/apirest_springboot/Startup.java
+++ 00 apirest_springboot/src/main/java/br/com/apirest_springboot/Startup.java
@@ -3,9 +3,15 @@ package br.com.apirest_springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
+import br.com.apirest_springboot.config.FileStorageConfig;
+
 @SpringBootApplication
+@EnableConfigurationProperties({
+	FileStorageConfig.class
+})
 @EnableAutoConfiguration
 @ComponentScan
 public class Startup {
---
+++ 00 apirest_springboot/src/main/java/br/com/apirest_springboot/config/FileStorageConfig.java
@@ -0,0 +1,19 @@
+package br.com.apirest_springboot.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "file")
+public class FileStorageConfig {
+
+	private String uploadDir;
+
+	public String getUploadDir() {
+		return uploadDir;
+	}
+
+	public void setUploadDir(String uploadDir) {
+		this.uploadDir = uploadDir;
+	}
+	
+	
+}
